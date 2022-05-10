cc021d9467ab7f0c43ed8537bea201a0cb22e8cc
SouL-H <mhmtaydinw@gmail.com>
2022-05-10 21:04:15 +0300

    get-paid near state line added

--- scripts/3.get-paid.sh
+++ scripts/3.get-paid.sh
@@ -5,10 +5,12 @@ echo "Hello $1"
 echo ---------------------------------------------------------
 
 near call $CONTRACT getPay '{"wallet": "'"$1"'"}' --accountId $1
-
+set -e
 
 echo 'The amount to be paid has been transferred to your account.'
 echo ---------------------------------------------------------
 echo
+
+near state $1 
 
 exit 0
--- scripts/4.student-info.sh
+++ scripts/4.student-info.sh
@@ -6,7 +6,7 @@ echo "The detailed information of the account is listed below."
 echo ---------------------------------------------------------
 
 near view $CONTRACT getInfo '{"wallet": "'"$1"'"}'
-
+set -e
 
 echo 'The amount to be paid has been transferred to your account.'
 echo ---------------------------------------------------------
